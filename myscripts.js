0a0fe0e859d0c9585b5a03c733b8335f53ede5b0
Chris Lee <cil5345@gmail.com>
2020-02-11 23:08:37 -0500

    assigned buttonsto the quiz

--- myscripts.js
+++ myscripts.js
@@ -8,6 +8,7 @@ let aAnswer = document.getElementById("choice-a");
 let bAnswer = document.getElementById("choice-b");
 let cAnswer = document.getElementById("choice-c");
 let dAnswer = document.getElementById("choice-d");
+let gameOverContainer = document.getElementById("end-game");
 
 // console.log(dQuestion);
 // console.log(timeEl);
@@ -18,22 +19,22 @@ let dAnswer = document.getElementById("choice-d");
 
 // ========================================================================
 var questions = [{
-    q:"What is the name of King Joffery's valyrian steel sword?",
-    a: "A",
+    q: "What is the name of King Joffery's valyrian steel sword?",
+    a: "a",
     c1: "Widow's Wail",
     c2: "Oath Keeper",
     c3: "Lion's Claw",
     c4: "Pointy"
 }, {
-    q:"What is John Snow's real name?",
-    a: "C",
+    q: "What is John Snow's real name?",
+    a: "c",
     c1: "Aerys Targaryen",
     c2: "John Stark",
     c3: "Aegon Targaryen",
     c4: "John Smith"
 }, {
-    q:"Which is the worst season of Game of Thrones?",
-    a: "D",
+    q: "Which is the worst season of Game of Thrones?",
+    a: "d",
     c1: "Season 1",
     c2: "Season 6",
     c3: "Season 2",
@@ -52,32 +53,69 @@ mainSubmitButton.addEventListener("click", function loadQuestion() {
     introContainer.style.visibility = "hidden";
     quizContainer1.style.visibility = "visible";
 
-    
+
 
     var timeLeft = 60;
     var timeInterval = setInterval(function () {
         timeEl.textContent = timeLeft + " seconds remaining";
         timeLeft--;
-       
-        
+
+
         if (timeLeft === 0) {
             timeEl.textContent = "";
-            
+
             clearInterval(timeInterval);
-            // endGame();
+            endGame();
         }
-        setQuestions();
+
     }, 1000);
 
+    setQuestions();
+
+
+
+
     function setQuestions() {
-        var objQues = questions[0];
+        var i = 0;
+        var objQues = questions[i];
+        var correctAnswer = objQues.a;
+        console.log(correctAnswer);
         quizQuestion.textContent = objQues.q;
         aAnswer.textContent = objQues.c1;
         bAnswer.textContent = objQues.c2;
         cAnswer.textContent = objQues.c3;
         dAnswer.textContent = objQues.c4;
-        // aQuestion.textContent = 
-    // console.log(questions
+
+        aAnswer.addEventListener('click', function () {
+            // console.log("hello")
+            console.log(objQues.c1);
+        });
+        bAnswer.addEventListener('click', function () {
+            // console.log("hello")
+            console.log(objQues.c2);
+        });
+        cAnswer.addEventListener('click', function () {
+            // console.log("hello")
+            console.log(objQues.c3);
+        });
+        dAnswer.addEventListener('click', function () {
+            // console.log("hello")
+            console.log(objQues.c4);
+        });
+
+
+        if (correctAnswer === true) {
+            console.log(correctAnswer);
+        }
+    }
+
+
+  
+
+    function endGame() {
+        gameOverContainer.style.visibility = "visible";
+        quizContainer1.style.visibility = "hidden";
+
     }
     // Starts a timer
     // var timeLeft = 60;
